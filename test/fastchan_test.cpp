d57b00049e968de3a761ef8e4a382d99ff92e83b
E Sequeira <5458743+geseq@users.noreply.github.com>
2023-02-05 21:37:06 +0000

    Update fastchan_test.cpp

--- test/fastchan_test.cpp
+++ test/fastchan_test.cpp
@@ -54,7 +54,7 @@ int main() {
         }
     });
 
-    std::thread consumer([&] {
+    std::thread consumer([&](fastchan::FastChan<int, chan_size>& chan) {
         for (int i = 0; i < iterations * 2; ++i) {
             std::cout << "GET" << i << std::endl;
             assert(chan.get() == i);
