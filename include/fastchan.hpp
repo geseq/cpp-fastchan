c2b41452342d7576dc72d2c4e4fd047d104101f9
E Sequeira <5458743+geseq@users.noreply.github.com>
2023-02-09 15:19:51 +0000

    non-blocking tests and performance improvements (#11)
    * improve non-blocking puts and gets
    
    * add tests for non-blocking gets and puts
    
    * improve performance

--- include/fastchan.hpp
+++ include/fastchan.hpp
@@ -9,7 +9,7 @@ namespace fastchan {
 #define CHAR_BIT __CHAR_BIT__
 #endif
 
-enum BlockingType { NonBlocking, BlockingPut, BlockingGet, BlockingBoth };
+enum BlockingType { NonBlocking, NonBlockingPut, NonBlockingGet, Blocking };
 
 constexpr size_t roundUpNextPowerOfTwo(size_t v) {
     v--;
@@ -24,35 +24,38 @@ class FastChan {
    public:
     FastChan() : last_committed_index_(0), next_free_index_(1), reader_index_(1) {}
 
-    void put(const T &value) noexcept {
-        auto my_index = next_free_index_.fetch_add(1, std::memory_order_acq_rel);
+    bool put(const T &value) noexcept {
+        auto my_index = next_free_index_.load(std::memory_order_relaxed);
         while (my_index > (reader_index_.load(std::memory_order_acquire) + index_mask_)) {
-            if (blocking == NonBlocking || blocking == BlockingGet) {
-                return;
+            if (blocking == NonBlocking || blocking == NonBlockingPut) {
+                return false;
             }
             std::this_thread::yield();
         }
 
+        next_free_index_.store(my_index + 1, std::memory_order_relaxed);
         contents_[my_index & index_mask_] = value;
         last_committed_index_.store(my_index, std::memory_order_release);
+        return true;
     }
 
     T get() noexcept {
-        while (reader_index_.load(std::memory_order_acquire) > last_committed_index_.load(std::memory_order_acquire)) {
-            if (blocking == NonBlocking || blocking == BlockingPut) {
+        auto my_index = reader_index_.load(std::memory_order_relaxed);
+        while (my_index > last_committed_index_.load(std::memory_order_acquire)) {
+            if (blocking == NonBlocking || blocking == NonBlockingGet) {
                 return T();
             }
             std::this_thread::yield();
         }
 
-        auto contents = contents_[reader_index_.load(std::memory_order_acquire) & index_mask_];
-        reader_index_.fetch_add(1, std::memory_order_acq_rel);
+        auto contents = contents_[my_index & index_mask_];
+        reader_index_.store(my_index + 1, std::memory_order_release);
         return contents;
     }
 
     void empty() noexcept {
-        last_committed_index_.store(0, std::memory_order_release);
         next_free_index_.store(1, std::memory_order_release);
+        last_committed_index_.store(0, std::memory_order_release);
         reader_index_.store(1, std::memory_order_release);
     }
 
@@ -60,13 +63,13 @@ class FastChan {
 
     bool isEmpty() const noexcept { return reader_index_.load(std::memory_order_acquire) > last_committed_index_.load(std::memory_order_acquire); }
 
-    bool isFull() const noexcept { return next_free_index_.load(std::memory_order_acquire) > (reader_index_.load(std::memory_order_acquire) + index_mask_); }
+    bool isFull() const noexcept { return next_free_index_ > (reader_index_.load(std::memory_order_acquire) + index_mask_); }
 
    private:
     const std::size_t index_mask_ = roundUpNextPowerOfTwo(min_size) - 1;
-    alignas(64) std::atomic<std::size_t> last_committed_index_;
-    alignas(64) std::atomic<std::size_t> next_free_index_;
-    alignas(64) std::atomic<std::size_t> reader_index_;
+    alignas(64) std::atomic<std::size_t> last_committed_index_{0};
+    alignas(64) std::atomic<std::size_t> reader_index_{0};
+    alignas(64) std::atomic<std::size_t> next_free_index_{0};
     alignas(64) std::array<T, roundUpNextPowerOfTwo(min_size)> contents_;
 };
 
--- test/fastchan_test.cpp
+++ test/fastchan_test.cpp
@@ -1,19 +1,20 @@
 #include <cassert>
 #include <fastchan.hpp>
+#include <iostream>
 #include <thread>
 
 using namespace std::chrono_literals;
 
-int main() {
-    constexpr int iterations = 4096;  // power of 2
+template <fastchan::BlockingType blockingType, int iterations>
+void testFastchanSingleThreaded() {
     constexpr std::size_t chan_size = (iterations / 2) + 1;
-    fastchan::FastChan<int, chan_size, fastchan::BlockingBoth> chan;
+    fastchan::FastChan<int, chan_size, blockingType> chan;
 
     assert(chan.size() == 0);
     assert(chan.isEmpty() == true);
     // Test put and read with a single thread
     for (int i = 0; i < iterations; ++i) {
-        chan.put(i);
+        assert(chan.put(i));
 
         assert(chan.size() == i + 1);
         assert(chan.isEmpty() == false);
@@ -33,27 +34,48 @@ int main() {
 
     // Test put and get with a single thread
     for (int i = 0; i < iterations; ++i) {
-        chan.put(i);
+        assert(chan.put(i));
     }
 
     for (int i = 0; i < iterations; ++i) {
         assert(chan.get() == i);
     }
 
+    assert(chan.isEmpty());
     assert(chan.size() == 0);
     chan.empty();
     assert(chan.size() == 0);
+    assert(chan.isEmpty());
+}
+
+template <fastchan::BlockingType blockingType, int iterations>
+void testFastchanMultiThreaded() {
+    constexpr std::size_t chan_size = (iterations / 2) + 1;
+    fastchan::FastChan<int, chan_size, blockingType> chan;
 
     // Test put and get with multiple threads
     std::thread producer([&] {
-        for (int i = 0; i < iterations * 2; ++i) {
-            chan.put(i);
+        for (int i = 1; i <= iterations * 2; i++) {
+            auto result = chan.put(i);
+            if (blockingType == fastchan::NonBlocking || blockingType == fastchan::NonBlockingPut) {
+                while (!result) {
+                    result = chan.put(i);
+                }
+            }
         }
     });
 
     std::thread consumer([&] {
-        for (int i = 0; i < iterations * 2; ++i) {
-            assert(chan.get() == i);
+        for (int i = 1; i <= iterations * 2;) {
+            auto&& val = chan.get();
+            if (blockingType == fastchan::NonBlocking || blockingType == fastchan::NonBlockingGet) {
+                if (val == 0) {
+                    continue;
+                }
+            }
+
+            assert(val == i);
+            ++i;
         }
     });
 
@@ -61,16 +83,19 @@ int main() {
     consumer.join();
 
     assert(chan.size() == 0);
+}
 
-    // Test filling and then emptying the chan
-    for (int i = 0; i < iterations; ++i) {
-        chan.put(i);
-    }
-    assert(chan.isFull());
-    for (int i = 0; i < iterations; ++i) {
-        assert(chan.get() == i);
-    }
-    assert(chan.isEmpty());
+template <fastchan::BlockingType blockingType>
+void testFastChan() {
+    testFastchanSingleThreaded<blockingType, 4096>();
+    testFastchanMultiThreaded<blockingType, 4096>();
+}
+
+int main() {
+    testFastChan<fastchan::Blocking>();
+    testFastChan<fastchan::NonBlockingGet>();
+    testFastChan<fastchan::NonBlockingPut>();
+    testFastChan<fastchan::NonBlocking>();
 
     return 0;
 }
