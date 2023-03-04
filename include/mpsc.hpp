276d666f124a4596c00d3324a4526176e8190765
E Sequeira <5458743+geseq@users.noreply.github.com>
2023-03-04 17:58:14 +0000

    simplify mpsc api

--- include/mpsc.hpp
+++ include/mpsc.hpp
@@ -7,18 +7,26 @@
 
 namespace fastchan {
 
-template <typename T, size_t min_size>
+template <typename T, BlockingType blocking_type, size_t min_size>
 class MPSC {
    public:
+    using put_t = typename std::conditional<(blocking_type == BlockingPutBlockingGet || blocking_type == BlockingPutNonBlockingGet), void, bool>::type;
+    using get_t = typename std::conditional<(blocking_type == BlockingPutBlockingGet || blocking_type == NonBlockingPutBlockingGet), T, std::optional<T>>::type;
+
     MPSC() = default;
 
-    void put(const T &value) noexcept {
-        auto index = next_free_index_.fetch_add(1, std::memory_order_acq_rel);
+    put_t put(const T &value) noexcept {
+        auto index = next_free_index_.fetch_add(1, std::memory_order_relaxed);
         while (index > (reader_index_.load(std::memory_order_acquire) + index_mask_)) {
-            std::this_thread::yield();
+            if constexpr (blocking_type == BlockingPutBlockingGet || blocking_type == BlockingPutNonBlockingGet) {
+                std::this_thread::yield();
+            } else {
+                next_free_index_.fetch_sub(1, std::memory_order_relaxed);
+                return false;
+            }
         }
 
-        auto write_index = writer_index_.fetch_add(1, std::memory_order_acq_rel);
+        auto write_index = writer_index_.fetch_add(1, std::memory_order_relaxed);
         while (write_index > (reader_index_.load(std::memory_order_acquire) + index_mask_))
             ;
 
@@ -29,42 +37,19 @@ class MPSC {
             // commit in the correct order to avoid problems
             cache_idx = write_index;
         }
+
+        if constexpr (blocking_type != BlockingPutBlockingGet && blocking_type != BlockingPutNonBlockingGet) {
+            return true;
+        }
     }
 
-    bool putWithoutBlocking(const T &value) noexcept {
-        auto index = next_free_index_.fetch_add(1, std::memory_order_acq_rel);
-        if (index > (reader_index_.load(std::memory_order_acquire) + index_mask_)) {
-            next_free_index_.fetch_sub(1, std::memory_order_acq_rel);
-            return false;
-        }
-
-        auto write_index = writer_index_.fetch_add(1, std::memory_order_acq_rel);
-        while (write_index > (reader_index_.load(std::memory_order_acquire) + index_mask_))
-            ;
-
-        contents_[write_index & index_mask_] = value;
-
-        auto cache_idx = write_index;
-        while (!last_committed_index_.compare_exchange_weak(cache_idx, write_index + 1, std::memory_order_acq_rel, std::memory_order_acquire)) {
-            // commit in the correct order to avoid problems
-            cache_idx = write_index;
-        }
-        return true;
-    }
-
-    T get() noexcept {
-        while (reader_index_2_ >= last_committed_index_.load(std::memory_order_acquire)) {
-            std::this_thread::yield();
-        }
-
-        auto contents = contents_[reader_index_2_ & index_mask_];
-        reader_index_.store(++reader_index_2_, std::memory_order_release);
-        return contents;
-    }
-
-    std::optional<T> getWithoutBlocking() noexcept {
-        if (reader_index_2_ >= last_committed_index_.load(std::memory_order_acquire)) {
-            return std::nullopt;
+    get_t get() noexcept {
+        while (reader_index_2_ >= last_committed_index_.load(std::memory_order_relaxed)) {
+            if constexpr (blocking_type == BlockingPutBlockingGet || blocking_type == NonBlockingPutBlockingGet) {
+                std::this_thread::yield();
+            } else {
+                return std::nullopt;
+            }
         }
 
         auto contents = contents_[reader_index_2_ & index_mask_];
